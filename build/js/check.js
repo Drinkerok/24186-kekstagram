b42cf33366f308a7f2352bf230aca926473875a3
Drinkerok <dmkazantsev@gmail.com>
2016-03-22 23:59:58 +0300

    module1-task2

---
+++ build/js/check.js
@@ -0,0 +1,27 @@
+function sumOfMultiplicationOfArrays(a,b){
+  var sum = 0;
+  for (var i=a.length;i--;){
+    sum = a[i] * b[i];
+  }
+  return sum;
+}
+function getMessage(a,b){
+  switch (typeof(a)){
+    case 'boolean':
+      if (a){
+        return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+      } else {
+        return 'Переданное GIF-изображение не анимировано';
+      }
+    case 'number':
+      return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
+    case 'object':
+      if (typeof(b) == 'object'){
+        return 'Общая площадь артефактов сжатия: ' + sumOfMultiplicationOfArrays(a,b) + ' пикселей';
+      } else{
+        return 'Количество красных точек во всех строчках изображения: ' + (a + b);
+      }
+    default:
+      return 'Что-то пошло не так';
+  }
+}
